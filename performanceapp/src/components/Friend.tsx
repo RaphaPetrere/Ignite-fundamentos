4963289151d00ff2ab09d3640a070b6b13622d72
Raphael Cardoso <raphaelpetrere@gmail.com>
2022-01-23 12:47:52 -0300

    ✨ Using memo to prevent re-render of components that didn't change

--- performanceapp/src/components/Friend.tsx
+++ performanceapp/src/components/Friend.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Text } from 'react-native';
 
 interface Props {
   data: {
+    id: number;
     name: string;
     likes: number;
   }
 }
-export const Friend = ({ data }: Props) => {
+const FriendComponent = ({ data }: Props) => {
   return (
     <Text>
       {data.name} - Likes: {data.likes}
     </Text>
   )
 };
+
+export const Friend = memo(FriendComponent, (prevProps, nextProps) =>
+  Object.is(prevProps.data, nextProps.data)
+  //só re-renderiza se essa comparação de valores retornar falsa
+);
